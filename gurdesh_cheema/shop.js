f0d9a531a06bca09a0293f32c1c77642f16c7da6
Christine E <christineebeo@Christines-MacBook-Pro.local>
2020-12-12 21:39:00 -0500

    rm all

--- gurdesh_cheema/shop.js
+++
@@ -1,8 +0,0 @@
-function load_details() {
-    let loadmorebtn = document.querySelector('#loadmorebtn');
-    loadmorebtn.style.display = 'none';
-
-    let loadmore = document.querySelector('#loadmore');
-    loadmore.style.display = 'block';
-
-}
